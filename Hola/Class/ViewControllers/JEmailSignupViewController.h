9305a04d9687f803c3a91c801ae93b3405fd2683
Jin Wang <automnberry106@gmail.com>
2015-06-22 22:35:18 +0800

    Push notification and email registration

---
+++ Hola/Class/ViewControllers/JEmailSignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  JEmailSignupViewController.h
+//  Hola
+//
+//  Created by Wang Bing on 20/06/15.
+//  Copyright (c) 2015 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JEmailSignupViewController : UIViewController
+
+@end
